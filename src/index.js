fcec69bd6186d69ef3e81ef688a3cc54a834fc50
Irina Filitovich <irina.filitovich75@gmail.com>
2020-10-02 03:22:16 +0300

    feat: decided

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+    if (array.length === 0) return 0;
+    return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+    if (array.length === 0) return 0;
+    return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof array === 'undefined' || array.length === 0) return 0;
+    return array.reduce((res, current) => res + current) / array.length;
 }
